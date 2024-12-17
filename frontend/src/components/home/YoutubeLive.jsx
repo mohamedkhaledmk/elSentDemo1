1a4704ee06dcdd73bf81f11233fee452ce2a012f
Ibrahim ElTouny <ibrahimtouny559@gmail.com>
2024-12-18 00:21:58 +0200

    YouTube live

---
+++ frontend/src/components/home/YoutubeLive.jsx
@@ -0,0 +1,11 @@
+
+const YoutubeLive = () => {
+    return (
+        <div className="">
+            <iframe width="914" height="514" src="https://www.youtube.com/embed/YDvsBbKfLPA" title="Watch Sky News" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+    )
+}
+
+export default YoutubeLive;
+
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import HeroHome from "../components/home/HeroHome";
 import LiveHome from "../components/home/LiveHome";
 import ProcessHome from "../components/home/ProcessHome";
 import UpcommingHome from "../components/home/UpcommingHome";
-
+import YoutubeLive from "../components/home/YoutubeLive";
 import { register } from "swiper/element/bundle";
 // register Swiper custom elements
 register();
@@ -15,6 +15,7 @@ const Home = () => {
       <div className="px-5 lg:px-12 flex flex-col gap-20">
         <LiveHome />
         <UpcommingHome />
+        <YoutubeLive />
         <ProcessHome />
         <div className="text-white flex flex-col gap-8">
           <CreateEarnHome />
