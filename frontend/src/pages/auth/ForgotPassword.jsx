8afbaa52d056c92cc1c55fd63e47af8b6e0c3bf6
Yasmine Emad <yasminee455@gmail.com>
2024-12-20 02:24:49 +0200

    reset pass ready

--- frontend/src/pages/auth/ForgotPassword.jsx
+++ frontend/src/pages/auth/ForgotPassword.jsx
@@ -39,23 +39,23 @@ const ForgotPassword = () => {
   };
 
   return (
-    <div className="flex h-screen w-full items-center justify-center bg-[#061224] text-[#7386a8]">
-      <div className="flex w-[90%] flex-col items-center rounded-xl bg-[#071B36] py-8 sm:w-2/5 sm:px-6">
+    <div className="flex h-screen w-full items-center justify-center bg-theme-bg2 text-[#7386a8]">
+      <div className="flex w-[90%] flex-col items-center rounded-xl bg-body-bg py-8 sm:w-2/5 sm:px-6">
         <h1 className="text-3xl font-bold text-white">
-          <span className="uppercase text-[#00A3FF]">B</span>id
-          <span className="uppercase text-[#00A3FF]">F</span>air
+          <span className="uppercase text-theme-color">E</span>l
+          <span className="uppercase text-theme-color">S</span>ent
         </h1>
-        <p className="m-2 text-xl">Reset your account password</p>
+        <p className="m-2 text-xl text-theme-color">Reset your account password</p>
         <p className="my-3 h-[1px] w-[80%] bg-[#747d9340]"></p>
         <form
           className="flex w-[90%] flex-col sm:w-[90%]"
           onSubmit={handlePasswordReset}
         >
-          <label className="my-1 text-lg">Email Address</label>
+          <label className="my-1 text-lg text-body-text-color">Email Address</label>
           <input
             type="email"
             placeholder="Your Email"
-            className="focus:border-1 rounded text-white border-[1px] focus:border-[#00A3FF]  border-none focus:border-1 focus:border-solid bg-[#0E294D] px-5 py-3 outline-none  mb-2 placeholder-body-text-color"
+            className="focus:border-1 rounded text-white border-[1px] focus:border-[#00A3FF]  border-none focus:border-1 focus:border-solid bg-border-info-color px-5 py-3 outline-none  mb-2 placeholder-body-text-color"
             name="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
@@ -64,7 +64,7 @@ const ForgotPassword = () => {
 
           <button
             type="submit"
-            className="my-4 font-Roboto outline-none border-none w-full rounded bg-[#00A3FF] px-4 py-3 font-bold hover:bg-color-danger  text-[#ffffff]"
+            className="my-4 font-Roboto outline-none border-none w-full rounded bg-theme-bg px-4 py-3 font-bold hover:bg-color-danger  text-[#ffffff]"
           >
             Send Reset Link
           </button>
