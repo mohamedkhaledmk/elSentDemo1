2ebdd54f164e6058b57c762ae48a7570f2bf21e9
Yasmine Emad <yasminee455@gmail.com>
2024-12-20 09:03:28 +0200

    stocks

--- frontend/src/components/home/MetalPrices.jsx
+++ frontend/src/components/home/MetalPrices.jsx
@@ -3,45 +3,59 @@ import { getAllMetals } from "../../store/metals/metalsSlice";
 import { useEffect } from "react";
 
 const MetalPrices = () => {
-    const { metals } = useSelector((state) => state.metal);
-    
-    const dispatch = useDispatch();
+  const { metals } = useSelector((state) => state.metal);
 
-    useEffect(() => {
-        dispatch(getAllMetals());
-    }, [dispatch]);
+  const dispatch = useDispatch();
 
-    const formattedDate = new Date().toLocaleDateString();
+  useEffect(() => {
+    dispatch(getAllMetals());
+  }, [dispatch]);
 
-    console.log('MetalPrices thing:', { metals });
+  const formattedDate = new Date().toLocaleDateString();
 
-    return (
-        <div>
-            <h2>Metal Prices</h2>
-            {metals && metals.length ? (
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Metal</th>
-                            <th>Date</th>
-                            <th>Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {metals.map((metal) => (
-                            <tr key={metal._id}>
-                                <td>{metal.metal}</td>
-                                <td>{formattedDate}</td>
-                                <td>SAR{metal.price}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            ) : (
-                <div>No metal prices available.</div>
-            )}
+  return (
+    <div className="px-7 py-4 w-full bg-theme-bg text-slate-300 rounded-2xl">
+      <h2 className="text-white font-bold text-xl border-b border-border-info-color pb-3 mb-5">
+        Metal Prices Today
+      </h2>
+      {metals && metals.length ? (
+        <div className="overflow-auto px-4 rounded-2xl border border-border-info-color mt-4 max-h-[500px]">
+          <table className="text-left whitespace-nowrap w-full border-separate border-spacing-x-0 border-spacing-y-4">
+            <thead className="sticky top-0 bg-theme-bg table-header-group">
+              <tr>
+                <th className="p-2 pr-5 select-none first:rounded-l-lg last:rounded-r-lg border-b border-border-info-color hover:bg-theme-bg2 transition-all">
+                  Metal
+                </th>
+                <th className="p-2 pr-5 select-none first:rounded-l-lg last:rounded-r-lg border-b border-border-info-color hover:bg-theme-bg2 transition-all">
+                  Date
+                </th>
+                <th className="p-2 pr-5 select-none first:rounded-l-lg last:rounded-r-lg border-b border-border-info-color hover:bg-theme-bg2 transition-all">
+                  Price
+                </th>
+              </tr>
+            </thead>
+            <tbody className="table-row-group">
+              {metals.map((metal) => (
+                <tr key={metal._id} className="border">
+                  <td className="pl-2 pr-5 border-b border-border-info-color pb-2">
+                    {metal.metal}
+                  </td>
+                  <td className="pl-2 pr-5 border-b border-border-info-color pb-2">
+                    {formattedDate}
+                  </td>
+                  <td className="pl-2 pr-5 border-b border-border-info-color pb-2">
+                    SAR {metal.price}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+      ) : (
+        <div>No metal prices available.</div>
+      )}
+    </div>
+  );
 };
 
 export default MetalPrices;
