1d7c6c91290a2f9b5bd7ebc8ae01086373aaa7f8
Yasmine Emad <yasminee455@gmail.com>
2024-12-20 04:17:05 +0200

    impp

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -17,11 +17,9 @@ const Dashboard = () => {
   const { auction, isLoading, isError, isSuccess, message } = useSelector(
     (state) => state.auction
   );
-  //console.log(auctionData);
 
   useEffect(() => {
     dispatch(getAllAuctions());
-    //console.log("dispatched");
   }, []);
 
   useEffect(() => {
@@ -32,7 +30,7 @@ const Dashboard = () => {
     }
   }, [auction]);
 
-  //pagination part
+  // Pagination part
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage, setitemsPerPage] = useState(12);
   const indexOfLastItem = currentPage * itemsPerPage;
@@ -49,55 +47,64 @@ const Dashboard = () => {
     setCurrentPage(currentPage + 1);
   };
 
-  // 318f69a969db4f7599b7fbb5043e444e
-
   return (
-    <div className="flex flex-col min-h-screen w-full  bg-body-bg text-[#7386a8]">
-      <div className="">
-        <SearchLocationCategory />
+    <div className="flex flex-col gap-4 items-start mb-5">
+      <div className="flex items-center"><span className="flex absolute animate-ping flex rounded-full h-3 w-3 bg-color-warning"></span>
+          <span className="relative flex rounded-full h-3 w-3 bg-color-warning"></span>
+        <span>&nbsp;&nbsp;&nbsp;</span> {/* Adds 3 spaces */}
+        <h2 className="flex text-2xl font-bold text-white">Live Auctions</h2>
+        <div>
+          
+        </div>
       </div>
 
-      {isLoading ? (
-        <Loading />
-      ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-4 md:grid-cols-3 lg:grid-cols-4 max-w-[1400px] mx-auto">
-          {" "}
-          {auctionData &&
-            currentItems.map((item, index) => (
-              <div key={index}>
-                <SingleAuction
-                  name={item?.name}
-                  startingPrice={item?.startingPrice}
-                  image={item?.image}
-                  endTime={item?.endTime}
-                  startTime={item?.startTime}
-                  id={item?._id}
-                  status={item?.status}
-                  sellerImage={item?.seller?.profilePicture}
-                  sellerName={item?.seller?.fullName}
-                  sellerId={item?.seller?._id}
-                  bidLength={item?.bids?.length}
-                  winnerFullName={item?.winner?.bidder?.fullName}
-                  winnerProfilePicture={item?.winner?.bidder?.profilePicture}
-                  winnerBidAmount={item?.winner?.bidAmount}
-                  winnerBidTime={item?.winner?.bidTime}
-                />
-              </div>
-            ))}{" "}
+      <div className="flex flex-col min-h-screen w-full bg-body-bg text-[#7386a8]">
+        <div>
+          <SearchLocationCategory />
         </div>
-      )}
-      {auctionData && auctionData?.length !== 0 ? (
-        <Pagination
-          totalPosts={auctionData?.length}
-          postsPerPage={itemsPerPage}
-          paginate={paginate}
-          currentPage={currentPage}
-          nextPage={nextPage}
-          prevPage={prevPage}
-        />
-      ) : (
-        <></>
-      )}
+
+        {isLoading ? (
+          <Loading />
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-4 md:grid-cols-3 lg:grid-cols-4 max-w-[1400px] mx-auto">
+            {" "}
+            {auctionData &&
+              currentItems.map((item, index) => (
+                <div key={index}>
+                  <SingleAuction
+                    name={item?.name}
+                    startingPrice={item?.startingPrice}
+                    image={item?.image}
+                    endTime={item?.endTime}
+                    startTime={item?.startTime}
+                    id={item?._id}
+                    status={item?.status}
+                    sellerImage={item?.seller?.profilePicture}
+                    sellerName={item?.seller?.fullName}
+                    sellerId={item?.seller?._id}
+                    bidLength={item?.bids?.length}
+                    winnerFullName={item?.winner?.bidder?.fullName}
+                    winnerProfilePicture={item?.winner?.bidder?.profilePicture}
+                    winnerBidAmount={item?.winner?.bidAmount}
+                    winnerBidTime={item?.winner?.bidTime}
+                  />
+                </div>
+              ))}{" "}
+          </div>
+        )}
+        {auctionData && auctionData?.length !== 0 ? (
+          <Pagination
+            totalPosts={auctionData?.length}
+            postsPerPage={itemsPerPage}
+            paginate={paginate}
+            currentPage={currentPage}
+            nextPage={nextPage}
+            prevPage={prevPage}
+          />
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 };
