0758cad39c6fd19b8a1672471ab615a00fe86383
Yasmine Emad <yasminee455@gmail.com>
2024-12-24 18:25:56 +0200

    imp

--- frontend/src/admin/components/Footer.jsx
+++ frontend/src/admin/components/Footer.jsx
@@ -18,8 +18,8 @@ const Footer = () => {
             className="flex items-center mb-4 sm:mb-0 space-x-3 no-underline"
           >
             <h1 className="text-3xl font-bold text-white font-Roboto">
+              <span className="uppercase text-theme-color">M</span>z
               <span className="uppercase text-theme-color">B</span>id
-              <span className="uppercase text-theme-color">F</span>air
             </h1>
           </Link>
           <ul className="flex flex-wrap items-center text-sm font-medium text-white list-none">
