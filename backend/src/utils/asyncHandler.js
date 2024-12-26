566730806b61ded35cd8652693c32aa952438d9d
Ibrahim ElTouny <ibrahimtouny559@gmail.com>
2024-12-26 22:13:35 +0200

    asyncHandler

--- backend/src/utils/asyncHandler.js
+++ backend/src/utils/asyncHandler.js
@@ -1,7 +1,18 @@
-const asyncHandler = (reqestHandler) => {
-    return (req, res, next) => {
-       Promise.resolve(reqestHandler(req, res, next)).catch((err) => next(err));
-     };
-   };
-   export { asyncHandler };
-   
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+      Promise.resolve(requestHandler(req, res, next)).catch((err) => {
+          if (res.headersSent) {
+              console.error('Headers already sent:', err);
+              return;
+          }
+          if (typeof next === 'function') {
+              next(err);
+          } else {
+              console.error(err);
+              res.status(500).send('Internal Server Error');
+          }
+      });
+  };
+};
+
+export { asyncHandler };
