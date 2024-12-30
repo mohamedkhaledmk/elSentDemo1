1734207e84ae2e55cef1dd19cc3622b5f8d3aea3
Yasmine Emad <yasminee455@gmail.com>
2024-12-30 03:50:48 +0200

    calc front

--- frontend/src/components/Calc.jsx
+++ frontend/src/components/Calc.jsx
@@ -22,7 +22,10 @@ const Calc = () => {
   const [color, setColor] = useState("");
   const [diamondPrice, setDiamondPrice] = useState("");
   const [goldBasePrice, setGoldBasePrice] = useState(""); // Base price for diamond calculations
-
+const [cut, setCut] = useState("");
+const [design, setDesign] = useState(""); 
+const [flourescence, setFlourescence] = useState("");
+const [setting, setSetting] = useState("");
   const goldPurities = [
     { label: "24k", multiplier: 1 },
     { label: "22k", multiplier: 0.9167 },
@@ -190,18 +193,32 @@ const Calc = () => {
 
       {activeTab === "diamond" && (
         <div>
-          <div className="carat-weight-input mb-4">
-            <input
-              type="number"
-              placeholder="Enter carat weight (1-15)"
-              value={caratWeight}
-              onChange={(e) => setCaratWeight(e.target.value)}
-              min={1}
-              max={15}
-              className="w-full p-2 rounded-lg"
-              style={{ backgroundColor: "rgba(224, 224, 224, 0.33)",color: "white" }}
-            />
-          </div>
+          <div className="carat-weight-dropdown mb-4">
+  <select
+    value={caratWeight}
+    onChange={(e) => setCaratWeight(e.target.value)}
+    className="w-full p-2 rounded-lg"
+    style={{ backgroundColor: "rgba(224, 224, 224, 0.33)", color: "white" }}
+  >
+    <option value="" disabled>Select carat weight</option>
+    <option value="0.01-0.03" style={{ color: "black" }}>0.01-0.03</option>
+    <option value="0.04-0.07" style={{ color: "black" }}>0.04-0.07</option>
+    <option value="0.08-0.14" style={{ color: "black" }}>0.08-0.14</option>
+    <option value="0.15-0.17" style={{ color: "black" }}>0.15-0.17</option>
+    <option value="0.23-0.29"style={{ color: "black" }}>0.23-0.29</option>
+    <option value="0.30-0.39" style={{ color: "black" }}>0.30-0.39</option>
+    <option value="0.40-0.49" style={{ color: "black" }}>0.40-0.49</option>
+    <option value="0.50-0.69" style={{ color: "black" }}>0.50-0.69</option>
+    <option value="0.70-0.89" style={{ color: "black" }}>0.70-0.89</option>
+    <option value="1.00-1.49" style={{ color: "black" }}>1.00-1.49</option>
+    <option value="1.50-1.99" style={{ color: "black" }}>1.50-1.99</option>
+    <option value="2.00-2.99" style={{ color: "black" }}>2.00-2.99</option>
+    <option value="3.00-3.99" style={{ color: "black" }}>3.00-3.99</option>
+    <option value="4.00-4.99" style={{ color: "black" }}>4.00-4.99</option>
+    <option value="5.00-5.99" style={{ color: "black" }}>5.00-5.99</option>
+    <option value="10.00-10.99" style={{ color: "black" }}>10.00-10.99</option>
+  </select>
+</div>
 
           <div className="cut-shape-dropdown mb-4">
             <select
@@ -256,9 +273,96 @@ const Calc = () => {
               <option value="H" style={{ color: "black" }}>H</option>
               <option value="I" style={{ color: "black" }}>I</option>
               <option value="J" style={{ color: "black" }}>J</option>
+              <option value="K" style={{ color: "black" }}>K</option>
+              <option value="L" style={{ color: "black" }}>L</option>
+              <option value="M" style={{ color: "black" }}>M</option>
+              <option value="N" style={{ color: "black" }}>N</option>
             </select>
           </div>
 
+          <div className="cut-dropdown mb-4">
+            <select
+              onChange={(e) => setColor(e.target.value)}
+              value={cut}
+              className="w-full p-2 rounded-lg"
+              style={{ backgroundColor: "rgba(224, 224, 224, 0.33)" ,color: "white"}}
+            >
+              <option value="" disabled>
+                Select Cut
+              </option>
+              <option value="Ideal" style={{ color: "black" }}>Ideal</option>
+              <option value="Excellent" style={{ color: "black" }}>Excellent</option>
+              <option value="Very Good" style={{ color: "black" }}>Very Good </option>
+              <option value="Good" style={{ color: "black" }}>Good </option>
+              <option value="Fair" style={{ color: "black" }}>Fair</option>
+            </select>
+          </div>
+
+          <div className="design-dropdown mb-4">
+            <select
+              onChange={(e) => setColor(e.target.value)}
+              value={design}
+              className="w-full p-2 rounded-lg"
+              style={{ backgroundColor: "rgba(224, 224, 224, 0.33)" ,color: "white"}}
+            >
+              <option value="" disabled>
+                Select Design
+              </option>
+              <option value="Cad design" style={{ color: "black" }}>Cad design</option>
+              <option value="Master piece" style={{ color: "black" }}>Master piece</option>
+              <option value="Casting" style={{ color: "black" }}>Casting </option>
+              <option value="Mechanic" style={{ color: "black" }}>Mechanic </option>
+              <option value="Stamp" style={{ color: "black" }}>Stamp</option>
+            </select>
+          </div>
+
+          <div className="flourescence-dropdown mb-4">
+            <select
+              onChange={(e) => setColor(e.target.value)}
+              value={flourescence}
+              className="w-full p-2 rounded-lg"
+              style={{ backgroundColor: "rgba(224, 224, 224, 0.33)" ,color: "white"}}
+            >
+              <option value="" disabled>
+                Select Flourescence
+              </option>
+              <option value="Nonen" style={{ color: "black" }}>None</option>
+              <option value="Faint" style={{ color: "black" }}>Faint</option>
+              <option value="Medium" style={{ color: "black" }}>Medium </option>
+              <option value="Strong" style={{ color: "black" }}>Strong </option>
+              <option value="Very Strong" style={{ color: "black" }}>Very Strong</option>
+            </select>
+          </div>
+
+          <div className="setting-dropdown mb-4">
+            <select
+              onChange={(e) => setColor(e.target.value)}
+              value={setting}
+              className="w-full p-2 rounded-lg"
+              style={{ backgroundColor: "rgba(224, 224, 224, 0.33)" ,color: "white"}}
+            >
+              <option value="" disabled>
+                Select Setting
+              </option>
+              <option value="prong" style={{ color: "black" }}>prong</option>
+              <option value="pave" style={{ color: "black" }}>pave</option>
+              <option value="micro" style={{ color: "black" }}>micro </option>
+              <option value="chanel" style={{ color: "black" }}>chanel </option>
+              <option value="bazel" style={{ color: "black" }}>bazel</option>
+              <option value="bars" style={{ color: "black" }}>bars</option>
+              <option value="invisibale" style={{ color: "black" }}>invisibale</option>
+            </select>
+          </div>
+          <div className="workmanship-input mb-4 text-white">
+            <input
+              type="number"
+              placeholder="Enter workmanship fee (SAR)"
+              value={workmanship}
+              onChange={handleWorkmanshipChange}
+              className="w-full p-2 rounded-lg"
+              style={{ backgroundColor: "rgba(224, 224, 224, 0.33)" }}
+            />
+          </div>
           <div id="display" className="calculator-display mb-4 text-white font-bold">
             {goldBasePrice || "0"}
           </div>
