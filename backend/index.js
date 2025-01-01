70c7363198372c32d5a7e51cb2dec47796d8e02a
Mohamed Khaled <mohamed181371@feng.bu.edu.eg>
2025-01-02 01:59:44 +0200

    app

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
 import dotenv from "dotenv";
-import { app } from "./src/app.js";
+// import { app } from "./src/app.js";
 import connectDB from "./src/db/index.js";
 import http from "http";
 import { init as io } from "./src/utils/socket.js";
 
-const server = http.createServer(app);
-
-const ioInstance = io(server);
-ioInstance.on("connection", (socket) => {
-  console.log("Socket is connected!");
-});
+const app = express();
+// const server = http.createServer(app);
+// const ioInstance = io(server);
+// ioInstance.on("connection", (socket) => {
+//   console.log("Socket is connected!");
+// });
 
 process;
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 dotenv.config({
   path: "./env",
 });
